e835f7bf5073e6cf251e65969a71fc8ddbb0ac7d
Morgane Geoffroy <morgane.geoffroy34@gmail.com>
2023-08-28 00:47:37 +0200

    Réorganisation de la branche et mise à jour des scripts

---
+++ scripts/explore/devScript.py
@@ -0,0 +1,113 @@
+""" Imports des librairies """
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import sys
+from scripts.models import MongoDBSingleton
+from scripts.utils import ALL_COLL, CAT_COLUMNS, NUM_COLUMNS
+from scipy.stats import shapiro, bartlett
+from tableone import TableOne
+
+
+""" Accession au singleton """
+tmp_path = os.getcwd().split("MedicalAssistant_V2")[0]
+target_path = os.path.join(tmp_path, 'MedicalAssistant_V2')
+sys.path[:0] = [target_path]
+
+""" Création d'une instance """
+db = MongoDBSingleton.get_instance()
+
+""" 
+    Notes :
+    
+    - Variables catégorielles :
+        On réalise un Chi2.
+        Conditions d'application :
+            - Au  moins 5 individus dans chaque groupe (ok).
+
+    - Variables numériques :
+        On réalise un t-test.
+        Conditions d'application :
+            - Indépendance des deux groupes 
+            - Normalité
+            - Homoscédasticité
+"""
+
+""" Importation des données """
+data = pd.DataFrame(list(db.get_collection("heart").find({}, {'_id' : 0})))
+
+""" Réalisation préliminaire des tests statistiques univariés """
+mytable = TableOne(data, 
+                   columns = ALL_COLL, 
+                   categorical = [x for x in CAT_COLUMNS if x != 'HeartDisease'], 
+                   groupby = ['HeartDisease'],
+                   pval = True)
+
+print(mytable.tabulate(tablefmt = "fancy_grid"))
+mytable.to_excel('./scripts/explore/tests_préliminaires.xlsx')
+
+""" Variables qualitatives """
+"""
+    Le fichier mytable confirme que chacun des échantillons est constitué d'au moins 5 individus.
+    On regroupe les classes d'âge deux à deux afin de d'étoffer les effectifs des plus petites qui comprennent
+    environ 100 individus sur les 319 000 initiaux.
+    On recode le reste des variables en binaire pour celles à deux classes et on les one-hot encode 
+    lorsqu'elles en contiennent strictement plus de deux.
+"""
+
+""" Variables quantitatives """
+"""
+    Indépendance des échantillons :
+        - valeurs observées dans les différents groupes indépendantes les unes des autres.
+"""
+
+""" 
+    Normalité :
+        - Vérifiée visuellement
+        - Le Théorème Central Limite la suppose pour un nombre d'individus 
+          suffisament grand dans chaque groupe (> 30)
+        - Normalité vérifiée pour les variables BMI et SleepTime
+        - Normalité non vérifiée pour les variables PhysicalHealth et MentalHealth que l'on passe en catégorielles
+"""
+
+# Génération des graphiques
+fig, axs = plt.subplots(2, 2) # HeartDisease = Yes
+axs[0, 0].hist(data[data['HeartDisease'].isin(['Yes'])]['BMI'], edgecolor = 'black', bins = 50) ; axs[0, 0].set_title('BMI')
+axs[0, 1].hist(data[data['HeartDisease'].isin(['Yes'])]['PhysicalHealth'], edgecolor = 'black', bins = 50) ; axs[0, 1].set_title('PhysicalHealth')
+axs[1, 0].hist(data[data['HeartDisease'].isin(['Yes'])]['MentalHealth'], edgecolor = 'black', bins = 50) ; axs[1, 0].set_title('MentalHealth')
+axs[1, 1].hist(data[data['HeartDisease'].isin(['Yes'])]['SleepTime'], edgecolor = 'black', bins = 50) ; axs[1, 1].set_title('SleepTime')
+plt.show()
+
+fig, axs = plt.subplots(2, 2) # HeartDisease = No
+axs[0, 0].hist(data[data['HeartDisease'].isin(['No'])]['BMI'], edgecolor = 'black', bins = 50) ; axs[0, 0].set_title('BMI')
+axs[0, 1].hist(data[data['HeartDisease'].isin(['No'])]['PhysicalHealth'], edgecolor = 'black', bins = 50) ; axs[0, 1].set_title('PhysicalHealth')
+axs[1, 0].hist(data[data['HeartDisease'].isin(['No'])]['MentalHealth'], edgecolor = 'black', bins = 50) ; axs[1, 0].set_title('MentalHealth')
+axs[1, 1].hist(data[data['HeartDisease'].isin(['No'])]['SleepTime'], edgecolor = 'black', bins = 50) ; axs[1, 1].set_title('SleepTime')
+plt.show()
+
+# """ Ajustement des variables PhysicalHealth et MentalHealth """
+# data['PhysicalHealth_0'] = [1 if x == 0 else 0 for x in data['PhysicalHealth'].tolist()]
+# data['PhysicalHealth_1_29'] = [1 if x > 0 and x < 30 else 0 for x in data['PhysicalHealth'].tolist()]
+# data['PhysicalHealth_30'] = [1 if x == 30 else 0 for x in data['PhysicalHealth'].tolist()]
+# 
+# data['MentalHealth_0'] = [1 if x == 0 else 0 for x in data['MentalHealth'].tolist()]
+# data['MentalHealth_1_29'] = [1 if x > 0 and x < 30 else 0 for x in data['MentalHealth'].tolist()]
+# data['MentalHealth_30'] = [1 if x == 30 else 0 for x in data['MentalHealth'].tolist()]
+# 
+# data = data.drop(columns = ["PhysicalHealth", "MentalHealth"])
+
+""" 
+    Homoscédasticité :
+        - Egalité des variances dans les deux groupes
+        - Non vérifiée mais on s'en affranchi du fait que les échantillons n'ont pas été
+          créé de manière aléatoire mais sur la base d'une variable.
+"""
+# Test de Bartlett
+bartlett(data[data['HeartDisease'].isin(['Yes'])]["BMI"], data[data['HeartDisease'].isin(['No'])]["BMI"])
+# Différence significative des variances entre les deux groupes
+bartlett(data[data['HeartDisease'].isin(['Yes'])]["SleepTime"], data[data['HeartDisease'].isin(['No'])]["SleepTime"])
+# Différence significative des variances entre les deux groupes
+    
+
+
---
+++ scripts/explore/one_hot_encoding.py
@@ -0,0 +1,67 @@
+""" Imports des librairies """
+import os
+import pandas as pd
+import sys
+from scripts.models import MongoDBSingleton
+from scripts.utils import ALL_COLL
+from tableone import TableOne
+
+""" Accession au singleton """
+tmp_path = os.getcwd().split("MedicalAssistant_V2")[0]
+target_path = os.path.join(tmp_path, 'MedicalAssistant_V2')
+sys.path[:0] = [target_path]
+
+""" Création d'une instance """
+db = MongoDBSingleton.get_instance()
+
+""" Importation des données """
+data = pd.DataFrame(list(db.get_collection("heart").find({}, {'_id' : 0})))
+
+""" Transformations """
+
+for i in ALL_COLL :
+    if i in ['HeartDisease', 'Smoking', 'AlcoholDrinking', 'Stroke', 'DiffWalking', 'Sex', 'PhysicalActivity', 'Asthma', 'KidneyDisease', 'SkinCancer'] :
+        data[i] = [1 if x in ['Yes', 'Female'] else 0 for x in data[i].tolist()]
+        data = data.drop(columns = i)
+    elif i == 'AgeCategory' :
+        for j in range(0, len(data['AgeCategory'])) :
+            if data['AgeCategory'][j] in ['18-24','25-29'] : data['AgeCategory'][j] == '18-29'
+            elif data['AgeCategory'][j] in ['30-34','35-39'] : data['AgeCategory'][j] == '30-39'
+            elif data['AgeCategory'][j] in ['40-44','45-49'] : data['AgeCategory'][j] == '40-49'
+            elif data['AgeCategory'][j] in ['50-54','55-59'] : data['AgeCategory'][j] == '50-59'
+            elif data['AgeCategory'][j] in ['60-64','65-69'] : data['AgeCategory'][j] == '60-69'
+            elif data['AgeCategory'][j] in ['70-74','75-79'] : data['AgeCategory'][j] == '70-79'
+            else : continue
+        for j in ['18-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80 or older'] :
+            data[f'{i}_{"_".join(j.split(" "))}'] = [1 if x == j else 0 for x in data[i].tolist()]
+        data = data.drop(columns = i)
+    elif i == 'Race' :
+        for j in ['American Indian/Alaskan Native', 'Asian', 'Black', 'Hispanic', 'Other', 'White'] :
+            data[f'{i}_{"_".join(j.split(" "))}'] = [1 if x == j else 0 for x in data[i].tolist()]
+        data = data.drop(columns = i)
+    elif i == 'Diabetic' :
+        for j in ['No', 'No, borderline diabetes', 'Yes', 'Yes (during pregnancy)'] :
+            data[f'{i}_{"_".join(j.split(" "))}'] = [1 if x == j else 0 for x in data[i].tolist()]
+        data = data.drop(columns = i)
+    elif i == 'GenHealth' :
+        for j in ['Excellent', 'Fair', 'Good', 'Poor', 'Very good'] :
+            data[f'{i}_{"_".join(j.split(" "))}'] = [1 if x == j else 0 for x in data[i].tolist()]
+        data = data.drop(columns = i)
+    elif i in ['PhysicalHealth', 'MentalHealth'] :
+        for j in range(0, len(data[i])) :
+            if data[i][j] == 0 : data[i][j] == '0'
+            elif data[i][j] in list(range(1, 30)) : data[i][j] == '1-29'
+            elif data[i][j] == 30 : data[i][j] == '30'
+            else : 
+                continue
+        for j in ['0', '1-29', '30'] :
+            data[f'{i}_{"_".join(j.split(" "))}'] = [1 if x == j else 0 for x in data[i].tolist()]
+        data = data.drop(columns = i)
+    else : 
+        continue
+
+# data.to_excel("assets/transformed_data.xlsx")
+
+# Relancer la transformation
+# Supprimer le to_excel
+# Faire le rapport
