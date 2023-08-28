182184ce4eefdc0d0887cad9c4c597eed710af6a
Morgane Geoffroy <morgane.geoffroy34@gmail.com>
2023-08-28 22:28:36 +0200

    Mise à jour de la fonction dataPreprocessing.py

--- scripts/explore/dataPreprocessing.py
+++ scripts/explore/dataPreprocessing.py
@@ -4,16 +4,16 @@ def dataPreprocessing() :
     import os
     import pandas as pd
     import sys
-    from scripts.models import MongoDBSingleton
-    from scripts.utils import ALL_COLL
-    from sklearn.preprocessing import StandardScaler
-    from tableone import TableOne
 
-    """ Accession au singleton """
+    """ - """
     tmp_path = os.getcwd().split("MedicalAssistant_V2")[0]
     target_path = os.path.join(tmp_path, 'MedicalAssistant_V2')
     sys.path[:0] = [target_path]
 
+    """ Imports des librairies """
+    from scripts.models import MongoDBSingleton
+    from scripts.utils import ALL_COLL
+    
     """ Création d'une instance """
     db = MongoDBSingleton.get_instance()
 
@@ -22,16 +22,18 @@ def dataPreprocessing() :
 
     """ Transformations """
     for i in ALL_COLL :
+        print(i)
         if i in ['HeartDisease', 'Smoking', 'AlcoholDrinking', 'Stroke', 'DiffWalking', 'Sex', 'PhysicalActivity', 'Asthma', 'KidneyDisease', 'SkinCancer'] :
-            data[i] = [1 if x in ['Yes', 'Female'] else 0 for x in data[i].tolist()]
+            data[i] = [1 if x in ['Yes', 'Male'] else 0 for x in data[i].tolist()]
         elif i == 'AgeCategory' :
             for j in range(0, len(data['AgeCategory'])) :
-                if data['AgeCategory'][j] in ['18-24','25-29'] : data['AgeCategory'][j] == '18-29'
-                elif data['AgeCategory'][j] in ['30-34','35-39'] : data['AgeCategory'][j] == '30-39'
-                elif data['AgeCategory'][j] in ['40-44','45-49'] : data['AgeCategory'][j] == '40-49'
-                elif data['AgeCategory'][j] in ['50-54','55-59'] : data['AgeCategory'][j] == '50-59'
-                elif data['AgeCategory'][j] in ['60-64','65-69'] : data['AgeCategory'][j] == '60-69'
-                elif data['AgeCategory'][j] in ['70-74','75-79'] : data['AgeCategory'][j] == '70-79'
+                if j%10000 == 0 : print(j)
+                if data.loc[j,'AgeCategory'] in ['18-24','25-29'] : data.loc[j,'AgeCategory'] = '18-29'
+                elif data.loc[j,'AgeCategory'] in ['30-34','35-39'] : data.loc[j,'AgeCategory'] = '30-39'
+                elif data.loc[j,'AgeCategory'] in ['40-44','45-49'] : data.loc[j,'AgeCategory'] = '40-49'
+                elif data.loc[j,'AgeCategory'] in ['50-54','55-59'] : data.loc[j,'AgeCategory'] = '50-59'
+                elif data.loc[j,'AgeCategory'] in ['60-64','65-69'] : data.loc[j,'AgeCategory'] = '60-69'
+                elif data.loc[j,'AgeCategory'] in ['70-74','75-79'] : data.loc[j,'AgeCategory'] = '70-79'
                 else : continue
             for j in ['18-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80 or older'] :
                 data[f'{i}_{"_".join(j.split(" "))}'] = [1 if x == j else 0 for x in data[i].tolist()]
@@ -50,13 +52,14 @@ def dataPreprocessing() :
             data = data.drop(columns = i)
         elif i in ['PhysicalHealth', 'MentalHealth'] :
             for j in range(0, len(data[i])) :
-                if data[i][j] == 0 : data[i][j] == '0'
-                elif data[i][j] in list(range(1, 30)) : data[i][j] == '1-29'
-                elif data[i][j] == 30 : data[i][j] == '30'
-                else : 
-                    continue
+                if data.loc[j, i] == 0 : data.loc[j, i] = '0'
+                elif data.loc[j, i] in list(range(1, 30)) : data.loc[j, i] = '1-29'
+                elif data.loc[j, i] == 30 : data.loc[j, i] = '30'
+                else : continue
             for j in ['0', '1-29', '30'] :
+                print(j)
                 data[f'{i}_{"_".join(j.split(" "))}'] = [1 if x == j else 0 for x in data[i].tolist()]
             data = data.drop(columns = i)
-        else : 
-            continue
+        else : continue
+
+    return(data)
--- scripts/machineLearning/knn.py
+++ scripts/machineLearning/knn.py
@@ -1,9 +1,13 @@
 """ Importation des librairies """
+import pandas as pd
 from scripts.explore.dataPreprocessing import dataPreprocessing
-from sklearn.model_selection import StratifiedShuffleSplit
+# from sklearn.model_selection import StratifiedShuffleSplit
 
 """ Importation des données """
 data = dataPreprocessing()
+
+""" Centrage-réduction des données """
+# newdf = pd.DataFrame(scale(data[data.columns[1:len(data.columns)]]), index = data.index, columns = data.columns[1:len(data.columns)])
 
 """ Définition des features et de la target """
 features = data.drop(columns = ['HeartDisease'], axis = 1)
@@ -20,17 +24,18 @@ target_sample = target.iloc[sample_indices]
 
 
 
+from scripts.explore.dataPreprocessing import dataPreprocessing
 
+# for train_index, test_index in sss.split(features, target):
+#     X_train, X_test = features[train_index], features[test_index]
+#     y_train, y_test = target[train_index], target[test_index]
+#     # rf.fit(X_train, y_train)
+#     # pred = rf.predict(X_test)
+#     # scores.append(accuracy_score(y_test, pred))
+# 
+# for train_index in sss.split(features, target):
+#     print(train_index)
+# 
 
-for train_index, test_index in sss.split(features, target):
-    X_train, X_test = features[train_index], features[test_index]
-    y_train, y_test = target[train_index], target[test_index]
-    # rf.fit(X_train, y_train)
-    # pred = rf.predict(X_test)
-    # scores.append(accuracy_score(y_test, pred))
-
-for train_index in sss.split(features, target):
-    print(train_index)
-
-
-target_sample.value_counts()
+for i in data.columns :
+    print(data[i].value_counts())
