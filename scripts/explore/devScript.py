18ce561751f1ad3acb53cb779e5a770ac2376275
HugoAlp <hugo.alpiste@gmail.com>
2023-08-30 10:40:15 +0200

    ouais2

--- scripts/explore/devScript.py
+++ scripts/explore/devScript.py
@@ -18,9 +18,9 @@ sys.path[:0] = [target_path]
 """ Création d'une instance """
 db = MongoDBSingleton.get_instance()
 
-""" 
+"""
     Notes :
-    
+
     - Variables catégorielles :
         On réalise un Chi2.
         Conditions d'application :
@@ -29,7 +29,7 @@ db = MongoDBSingleton.get_instance()
     - Variables numériques :
         On réalise un t-test.
         Conditions d'application :
-            - Indépendance des deux groupes 
+            - Indépendance des deux groupes
             - Normalité
             - Homoscédasticité
 """
@@ -37,10 +37,22 @@ db = MongoDBSingleton.get_instance()
 """ Importation des données """
 data = pd.DataFrame(list(db.get_collection("heart").find({}, {'_id' : 0})))
 
+""" Matrice de corrélations """
+df =  data[data.columns].replace({'Yes':1, 'No':0, 'Male':1,'Female':0,'No, borderline diabetes':'0','Yes (during pregnancy)':'1' })
+df = df.drop(columns = ["GenHealth", "AgeCategory", "Race", "Diabetic"])
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+correlation = df.corr().round(2)
+plt.figure(figsize = (14,7))
+sns.heatmap(correlation, annot = True, cmap = 'YlOrBr')
+plt.xticks(rotation=20, horizontalalignment='right')
+plt.show()
+
 """ Réalisation préliminaire des tests statistiques univariés """
-mytable = TableOne(data, 
-                   columns = ALL_COLL, 
-                   categorical = [x for x in CAT_COLUMNS if x != 'HeartDisease'], 
+mytable = TableOne(data,
+                   columns = ALL_COLL,
+                   categorical = [x for x in CAT_COLUMNS if x != 'HeartDisease'],
                    groupby = ['HeartDisease'],
                    pval = True)
 
@@ -52,7 +64,7 @@ mytable.to_excel('./scripts/explore/tests_préliminaires.xlsx')
     Le fichier mytable confirme que chacun des échantillons est constitué d'au moins 5 individus.
     On regroupe les classes d'âge deux à deux afin de d'étoffer les effectifs des plus petites qui comprennent
     environ 100 individus sur les 319 000 initiaux.
-    On recode le reste des variables en binaire pour celles à deux classes et on les one-hot encode 
+    On recode le reste des variables en binaire pour celles à deux classes et on les one-hot encode
     lorsqu'elles en contiennent strictement plus de deux.
 """
 
@@ -62,10 +74,10 @@ mytable.to_excel('./scripts/explore/tests_préliminaires.xlsx')
         - valeurs observées dans les différents groupes indépendantes les unes des autres.
 """
 
-""" 
+"""
     Normalité :
         - Vérifiée visuellement
-        - Le Théorème Central Limite la suppose pour un nombre d'individus 
+        - Le Théorème Central Limite la suppose pour un nombre d'individus
           suffisament grand dans chaque groupe (> 30)
         - Normalité vérifiée pour les variables BMI et SleepTime
         - Normalité non vérifiée pour les variables PhysicalHealth et MentalHealth que l'on passe en catégorielles
@@ -90,14 +102,14 @@ plt.show()
 # data['PhysicalHealth_0'] = [1 if x == 0 else 0 for x in data['PhysicalHealth'].tolist()]
 # data['PhysicalHealth_1_29'] = [1 if x > 0 and x < 30 else 0 for x in data['PhysicalHealth'].tolist()]
 # data['PhysicalHealth_30'] = [1 if x == 30 else 0 for x in data['PhysicalHealth'].tolist()]
-# 
+#
 # data['MentalHealth_0'] = [1 if x == 0 else 0 for x in data['MentalHealth'].tolist()]
 # data['MentalHealth_1_29'] = [1 if x > 0 and x < 30 else 0 for x in data['MentalHealth'].tolist()]
 # data['MentalHealth_30'] = [1 if x == 30 else 0 for x in data['MentalHealth'].tolist()]
-# 
+#
 # data = data.drop(columns = ["PhysicalHealth", "MentalHealth"])
 
-""" 
+"""
     Homoscédasticité :
         - Egalité des variances dans les deux groupes
         - Non vérifiée mais on s'en affranchi du fait que les échantillons n'ont pas été
@@ -108,6 +120,5 @@ bartlett(data[data['HeartDisease'].isin(['Yes'])]["BMI"], data[data['HeartDiseas
 # Différence significative des variances entre les deux groupes
 bartlett(data[data['HeartDisease'].isin(['Yes'])]["SleepTime"], data[data['HeartDisease'].isin(['No'])]["SleepTime"])
 # Différence significative des variances entre les deux groupes
-    
 
 
